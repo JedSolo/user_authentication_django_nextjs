9fb78387ae6234f5b765ba592474759257509266
Solomon Sarfo <solomon.sarfo@amalitech.com>
2024-09-04 15:59:39 +0000

    Move email credential to env variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,8 +98,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'solomondev045@gmail.com'
-EMAIL_HOST_PASSWORD = 'cgzhiyfouliemrqz'
+EMAIL_HOST_USER = getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
@@ -178,7 +178,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static')
 ]
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
