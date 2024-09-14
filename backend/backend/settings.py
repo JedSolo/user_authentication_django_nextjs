466542092b0975c278c5351af03df0b857566d56
Solomon Sarfo <solomon.sarfo@amalitech.com>
2024-09-14 21:05:10 +0000

    finishing up on backend setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -100,10 +100,11 @@ DATABASES = {
 # EMAIL CONFIGURATION
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465 #587
 EMAIL_HOST_USER = getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = True
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 
 # Domain Settings
 DOMAIN = getenv('DOMAIN')
