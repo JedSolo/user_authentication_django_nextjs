e9766590a44ad8ec713c455a0a9e169c3115ebe4
Solomon Sarfo <solomon.sarfo@amalitech.com>
2025-03-10 15:28:19 +0000

    All working features expect google signin

--- frontend/hooks/index.ts
+++ frontend/hooks/index.ts
@@ -2,3 +2,4 @@ export { default as useRegister } from './use-register';
 export { default as useLogin } from './use-login';
 export { default as useResetPassword } from './use-reset-password';
 export { default as useResetPasswordConfirm } from './use-reset-password-confirm';
+export { default as useSocialAuth } from './use-social-auth'
